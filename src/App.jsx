ec5f27c5f7776f303824112ba63a9e3548cb45e0
anshu <anshhufx@gmail.com>
2026-06-03 21:13:59 +0530

    Add more card

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,25 @@ import { Card } from "./components/Card";
 
 export const App = () => {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <Card />
+    <div className="border-x py-32 min-h-screen border-neutral-300 mx-auto max-w-4xl">
+      <div className="flex flex-col divide-y divide-neutral-300 border-y border-neutral-300">
+        <div className="grid grid-cols-2 divide-x divide-neutral-300">
+          <div className=" flex justify-center  py-16 hover:bg-neutral-300/40 transition-colors duration-200">
+            <Card />
+          </div>
+          <div className="flex justify-center py-16 hover:bg-neutral-300/40 transition-colors duration-200">
+            <Card />
+          </div>
+        </div>
+        <div className="grid grid-cols-2 divide-x divide-neutral-300">
+          <div className="flex justify-center py-16 hover:bg-neutral-300/40 transition-colors duration-200">
+            <Card />
+          </div>
+          <div className="flex justify-center py-16 hover:bg-neutral-300/40 transition-colors duration-200">
+            <Card />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,15 +2,32 @@ import { IconPlusFilled } from "@tabler/icons-react";
 
 export const Card = () => {
   return (
-    <div className="relative w-fit active:scale-95 transition-transform duration-200">
-      <svg
-        className="absolute -inset-8 w-87 h-125 pointer-events-none font-semibold"
-        viewBox="0 0 340 500"
-      >
-        <defs>
-          <path
-            id="cardPath"
-            d="
+    <div className="relative w-fit">
+      <PathLabel />
+      <div className="relative h-100 w-70 bg-neutral-100 rounded-4xl p-2 shadow-sm cursor-pointer">
+        <Info>
+          <TitleRow title="ICED MATCHA" price="10$" />
+          <Tags tags={["Matcha", "Milk", "Mint leaves", "Vanilla"]} />
+        </Info>
+        <ImageArea
+          img="/Iced_Matcha_Latte.png"
+          note="Guess what? Delivery is free till 02/07"
+        />
+        <AddButton />
+      </div>
+    </div>
+  );
+};
+
+const PathLabel = () => (
+  <svg
+    className="absolute -inset-8 w-87 h-125 pointer-events-none font-semibold"
+    viewBox="0 0 340 500"
+  >
+    <defs>
+      <path
+        id="cardPath"
+        d="
               M 50 20
               H 290
               A 30 30 0 0 1 320 50
@@ -21,57 +38,83 @@ export const Card = () => {
               V 50
               A 30 30 0 0 1 50 20
             "
-          />
-        </defs>
+      />
+    </defs>
 
-        <text fontSize="14" fill="#7DA24A" letterSpacing="2">
-          <textPath href="#cardPath">
-            COLORS: E5E5E5, ECFCCA, D7E5C4, 497D00
-            <tspan dx="90">FONT - INTER</tspan>
-          </textPath>
-        </text>
-      </svg>
-      <div className="relative h-100 w-70 bg-neutral-100 rounded-4xl p-2 shadow-sm cursor-pointer">
-        <div className="absolute w-full left-0 bottom-3 flex flex-col justify-between">
-          <div className="px-6 flex flex-col gap-1.5">
-            <div className="flex justify-between font-bold">
-              <div className="text-base">ICED MATCHA</div>
-              <div className="text-base">10$</div>
-            </div>
-            <div className="flex text-[9px] items-center justify-between font-medium">
-              <p className="bg-lime-600/20 text-lime-700 py-0.5 px-1.5 rounded-sm">
-                Matcha
-              </p>
-              <p className="bg-lime-600/20 text-lime-700 py-0.5 px-1.5 rounded-sm">
-                Milk
-              </p>
-              <p className="bg-lime-600/20 text-lime-700 py-0.5 px-1.5 rounded-sm">
-                Mint leaves
-              </p>
-              <p className="bg-lime-600/20 text-lime-700 py-0.5 px-1.5 rounded-sm">
-                Vanilla
-              </p>
-            </div>
-          </div>
+    <text fontSize="14" fill="#7DA24A" letterSpacing="2">
+      <textPath href="#cardPath">
+        COLORS: E5E5E5, ECFCCA, D7E5C4, 497D00
+        <tspan dx="90">FONT - INTER</tspan>
+      </textPath>
+    </text>
+  </svg>
+);
+const ImageArea = ({ img, note }) => {
+  return (
+    <>
+      <div className="relative w-full h-80 bg-lime-700/70 rounded-3xl pb-1 rounded-b-4xl overflow-hidden">
+        <div className="absolute top-0 left-0 w-full h-[94%] overflow-hidden rounded-3xl rounded-b-4xl">
+          <img
+            className="w-full h-full scale-200 object-cover"
+            src={img}
+            alt="Matcha image"
+          />
         </div>
-        <div className="relative w-full h-80 bg-lime-700/70 rounded-3xl pb-1 rounded-b-4xl flex items-end justify-center">
-          <p className="text-[10px] text-lime-100 font-light">
-            Guess what? Delivery is free till 02/07
+        {note && (
+          <p className="absolute bottom-0.5 left-0 right-0 text-center text-[10px] text-lime-100 font-light">
+            {note}
           </p>
-          <div className="absolute top-0 left-0 overflow-hidden rounded-3xl rounded-b-4xl">
-            <img
-              className="w-70 scale-200 object-cover h-74"
-              src="/Iced_Matcha_Latte.png"
-              alt="Matcha image"
-            />
-            <div className="absolute top-4 right-4 bg-neutral-100 rounded-full p-1.5 cursor-pointer active:scale-95 transition-transform duration-200">
-              <div className="text-lime-700">
-                <IconPlusFilled size={18} />
-              </div>
-            </div>
-          </div>
-        </div>
+        )}
       </div>
+      {/* <div className="flex justify-between font-bold w-full">
+        <p className="text-base text-neutral-800">{title}</p>
+        <p className="text-base text-neutral-800">{price}</p>
+      </div>
+      <div className="flex text-[9px] items-center justify-between font-medium">
+        {tags.map((tag) => (
+          <p
+            key={tag}
+            className="bg-lime-600/20 text-lime-700 py-0.5 px-1.5 rounded-sm"
+          >
+            {tag}
+          </p>
+        ))}
+      </div> */}
+    </>
+  );
+};
+const AddButton = ({ onClick }) => (
+  <button
+    className="absolute top-4 right-4 bg-neutral-100 rounded-full p-1.5 cursor-pointer active:scale-95 transition-transform duration-200"
+    onClick={onClick}
+  >
+    <IconPlusFilled size={18} className="text-lime-700" />
+  </button>
+);
+const TitleRow = ({ title, price }) => {
+  return (
+    <div className="flex justify-between font-bold w-full">
+      <p className="text-base text-neutral-800">{title}</p>
+      <p className="text-base text-neutral-800">{price}</p>
+    </div>
+  );
+};
+const Info = ({ children }) => (
+  <div className="absolute w-full px-6 gap-1.5 left-0 bottom-3 flex flex-col justify-between">
+    {children}
+  </div>
+);
+const Tags = ({ tags }) => {
+  return (
+    <div className="flex text-[9px] items-center justify-between font-medium">
+      {tags.map((tag) => (
+        <p
+          key={tag}
+          className="bg-lime-600/20 text-lime-700 py-0.5 px-1.5 rounded-sm"
+        >
+          {tag}
+        </p>
+      ))}
     </div>
   );
 };
